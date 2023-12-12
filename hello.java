7354dfc16638598e8bc851224ef63ebd377c075d
ABHISHEK KUMAR MAURYA <120729207+abhishekmaurya2028@users.noreply.github.com>
2023-12-12 18:40:19 +0530

    4A Watermelon Problem

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+ 
+        if (x < 4) {
+            System.out.println("NO");
+        } 
+        else {
+            System.out.println(x % 2 == 0 ? "YES" : "NO");
+        }
+    }
+}
